0c49bd49f1b6a907ae61f9c1a2e57145d46aaf93
aboncic <41088288+aboncic@users.noreply.github.com>
2019-07-01 17:17:48 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,17 @@ Route::get('profile', 'UserController@profile');
 Route::post('profile', 'UserController@update_avatar');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('ajaxRequest', 'PostsController@ajaxRequest')->name('ajaxRequest');
-Route::get('/command',function(){
-    Artisan::call('storage:link');
-  });
+
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('public/' . $filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    return $response;
+});
 
