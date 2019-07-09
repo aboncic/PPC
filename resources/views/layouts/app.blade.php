73acc8075fb04e9049f84fe82f816fffc0ccd2eb
aboncic <41088288+aboncic@users.noreply.github.com>
2019-07-09 11:46:09 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,8 @@
                             <li class="nav-item avatar">
                                 <img src="/storage/avatars/{{ Auth::user()->avatar }}" class="rounded-circle z-depth-0" alt="avatar" height="35"/>
                                 <img src="{{route('avatar',Auth::user()->avatar)}}" class="rounded-circle z-depth-0" alt="avatar" height="35"/>
+                                <img src="/avatars/{{ Auth::user()->avatar }}" />
+
 
                             </li>
                             <li class="nav-item dropdown">
